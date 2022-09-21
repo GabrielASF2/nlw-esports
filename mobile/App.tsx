70c4aafc6544acee71d9bbf29fc0570b264a940f
GabrielASF2 <gabriel_asf2@hotmail.com>
2022-09-21 15:45:59 -0300

    Landing Page Web Finalizada

---
+++ mobile/App.tsx
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        NLW - E-Sports
+      </Text>
+
+      <Button title='Send 2'/>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+interface ButtonProps {
+  title: string;
+}
+
+
+function Button (props: ButtonProps){
+  return(
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
