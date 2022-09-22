f60a48e12af6d4fd4f571d29351dfd36e7c011d8
GabrielASF2 <gabriel_asf2@hotmail.com>
2022-09-22 09:39:52 -0300

    Mobile React Native 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>
-        NLW - E-Sports
-      </Text>
-
-      <Button title='Send 2'/>
-
-      <StatusBar style="auto" />
+    <View>
+      
     </View>
   );
 }
-interface ButtonProps {
-  title: string;
-}
 
 
-function Button (props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
