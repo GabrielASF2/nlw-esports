8a181e0996d625c2a1b082f871379636ffe872d8
GabrielASF2 <gabriel_asf2@hotmail.com>
2022-10-04 16:56:54 -0300

    DB Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,51 @@
 import express, { response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient({
+    log: ['query']
+});
 
-app.get('/games', (req , res) => {
-    return res.json([]);
+app.get('/games', async (req , res) => {
+    const games = await prisma.game.findMany({
+        include:{
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    });
+    
+    return res.json(games);
 });
 
 app.post('/ads', (req , res) => {
     return res.status(201).json([]);
 });
 
-app.get('/games/:id/ads', (req, res) =>{
-    // const gameId = request.params.id;
+app.get('/games/:id/ads', async (req, res) =>{
+    const gameId = req.params.id;
 
-    return res.json([
-            {id: 1, name: 'Anuncio 1'},
-            {id: 2, name: 'Anuncio 2'},
-            {id: 3, name: 'Anuncio 3'},
-            {id: 4, name: 'Anuncio 4'},
-            {id: 5, name: 'Anuncio 5'}
-        ])
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true ,
+            name: true ,
+            weekDays: true ,
+            useVoiceChannel: true ,
+            yearsPlaying: true ,
+            hoursStart: true ,
+            hoursEnd: true ,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    })
+
+    return res.json(ads)
 });
 
 app.get('/ads/:id/discord', (req, res) =>{
